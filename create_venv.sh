fdda879dcfa38c9738dead0a83c3dafb8a308992
Jarek Karwowski <chromedivizer@gmail.com>
2023-12-22 14:27:14 +0100

    `create_venv` script assigns explicit versions of modules
    These changes are required on the TIAGo's computer, otherwise "ModuleNotFoundError: No module named 'error'" error shows up during the launch

--- create_venv.sh
+++ create_venv.sh
@@ -5,21 +5,22 @@
 SCRIPT_DIR=$(realpath $(dirname $0))
 
 cd $SCRIPT_DIR
-virtualenv .venv
+virtualenv --python="python2.7" .venv
 source .venv/bin/activate
 
 # Make sure that installing with python into the venv: https://stackoverflow.com/a/5979776
-VENV_PYTHON=$SCRIPT_DIR/.venv/bin/python
-VENV_PIP=$SCRIPT_DIR/.venv/bin/pip
+# Valid setup consists of Python 2.7.17 and pip 20.3.4
+VENV_PYTHON=$SCRIPT_DIR/.venv/bin/python2.7
+VENV_PIP=$SCRIPT_DIR/.venv/bin/pip2.7
 
 # installing package dependencies
-$VENV_PIP install torch
-$VENV_PIP install torchvision
-$VENV_PIP install scipy
-$VENV_PIP install gym
-$VENV_PIP install configparser
-$VENV_PIP install matplotlib
-$VENV_PIP install gitpython
+$VENV_PIP install torch==1.4.0
+$VENV_PIP install torchvision==0.5.0
+$VENV_PIP install scipy==1.2.3
+$VENV_PIP install gym==0.16.0
+$VENV_PIP install configparser==4.0.2
+$VENV_PIP install matplotlib==2.2.5
+$VENV_PIP install gitpython==2.1.15
 
 # prepare RVO
 cd $SCRIPT_DIR/Python-RVO2/
